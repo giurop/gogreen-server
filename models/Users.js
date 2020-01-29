cb1a7b1944eed863ad1465e0ff4670407ab32adb
Giu Roperto <giu.roperto@gmail.com>
2020-01-29 13:45:48 -0300

    models adjusted

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const Recipe = require('./Recipes');
 
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  favourites: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
 });
 
 const User = mongoose.model('User', userSchema);
