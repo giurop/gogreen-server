2b46e0441d8ee55170e0980c39ec6728b0f041e5
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-01-30 20:03:45 -0300

    Recipes main routes done

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const passport = require('passport');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bcryptSalt = 10;
 
 const User = require('../models/Users');
